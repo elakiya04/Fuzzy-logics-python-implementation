edf925b98dea64f6c7af4b479642f1ccd0836f38
Elakiya <65751487+elakiya04@users.noreply.github.com>
2021-11-26 16:56:14 +0530

    Update transitivit_reflexivity_symmetry.py

--- transitivit_reflexivity_symmetry.py
+++ transitivit_reflexivity_symmetry.py
@@ -1,17 +1,25 @@
 import numpy as np
 def symmetric(A): 
   (m,n)=np.shape(A)
+  x=[]
   for i in range(0, m): 
     for j in range(0, n): 
       if (A[i, j])==(A[j, i]): 
+        x.append(True)
+      else: x.append(False)
+      if all(x)==True: 
         a=True
       else: a=False
   return a
 def reflexive(A): 
   (m,n)=np.shape(A)
+  y=[]
   for i in range(0, m): 
     for j in range(0, n): 
       if all(A[i])==all(A[j]): 
+        y.append(True)
+      else:y.append(False)
+      if all(y)==True: 
         b=True
       else: b=False
   return b
@@ -20,11 +28,11 @@ def transitive(A):
     c=True
   else: c=False
   return c
-A= np.array([[1, 0.4, 0, 0.2, 0.3],
-    [0.6, 1 ,0.4, 0, 0.8], 
-    [0, 0.4, 1, 0, 0], 
-    [0.2, 0, 0, 1, 0.5], 
-    [0.3, 0.8, 0, 0.5, 1]])
+A = np.array([[1, 0.4, 0.8, 0.2, 0.3],
+    [0.6, 1 ,0, 0, 0.8], 
+    [0, 0.4, 1, 0.8, 0.4], 
+    [0.2, 0, 0, 0.9, 0.5], 
+    [0.3, 0.8, 0.9, 0.5, 1]])
 print("Symmetry is", symmetric(A))
 print("Refelexitivity is",reflexive(A))
 print("Thus, Transitivity is",transitive(A))
